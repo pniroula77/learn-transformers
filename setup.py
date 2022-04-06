b6e883014364e9040e2cebaa5f5c576160702b11
Pawan Niroula <devpawan@pop-os.localdomain>
2022-04-06 22:46:30 +0545

    Initialized structure through cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Learn the downstream tasks using Transformer models.',
+    author='Pawan Niroula',
+    license='MIT',
+)
